a1af4a15d6f78729c2c43e789693521fec3bfe44
PavelEgorov-ru <pkotofffff@yandex.ru>
2026-05-20 08:38:46 +0300

    закончил урок 07 фазы 0

--- scratch/lesson-07/app/main.py
+++ scratch/lesson-07/app/main.py
@@ -7,7 +7,7 @@ from rich.panel import Panel
 
 def main() -> None:
     console = Console()
-    message = f"""Привет из твоего первгого контейнера!
+    message = f"""Привет из твоего первого контейнера!
 У тебя установлен python - {sys.version} на компьютере - {socket.gethostname()}"""
     console.print(Panel(message))
 
